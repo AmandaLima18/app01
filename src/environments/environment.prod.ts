afb676f7f3b2f839e06805cf2c47b5b758cf83fb
Nathaliaaguiar <59691090+Nathaliaaguiar@users.noreply.github.com>
2021-04-26 16:43:23 -0300

    Ajustes iniciais

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAa1FIKt7Xps0mpGsTRe2YlAflFRMROmy8",
+    authDomain: "primeiro-projeto-30214.firebaseapp.com",
+    projectId: "primeiro-projeto-30214",
+    storageBucket: "primeiro-projeto-30214.appspot.com",
+    messagingSenderId: "668615004798",
+    appId: "1:668615004798:web:c8a380418e866eb1593e2f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase: {
+    apiKey: "AIzaSyAa1FIKt7Xps0mpGsTRe2YlAflFRMROmy8",
+    authDomain: "primeiro-projeto-30214.firebaseapp.com",
+    projectId: "primeiro-projeto-30214",
+    storageBucket: "primeiro-projeto-30214.appspot.com",
+    messagingSenderId: "668615004798",
+    appId: "1:668615004798:web:c8a380418e866eb1593e2f"
+  }
 };
 
 /*
@@ -14,3 +22,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
